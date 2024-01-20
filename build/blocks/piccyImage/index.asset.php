f0b455423ba1c62555828d887c881d42e36a6a65
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-20 16:16:07 +0100

    Added if image exist to Piccy Image

--- build/blocks/piccyImage/index.asset.php
+++ build/blocks/piccyImage/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '94612719f24451bb27be');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '58a09de8923dab72111b');
